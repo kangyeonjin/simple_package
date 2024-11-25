ebf6a5fde4254a9edeba05d2aa47db5a6f371f8a
yeonjin <yeonjin917n.n@gmail.com>
2024-11-25 21:53:11 +0900

    commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="simple_package_yeonjin",
+    name="simple_package_yeonjinkkk",
     version="0.1.0",
     packages=find_packages(),
     description="A simple package for reversing strings",
